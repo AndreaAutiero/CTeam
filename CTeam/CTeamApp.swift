174b5bdf33e921f48cf5a3ac51af25a4dca33fd5
Andrea <giovanni.ga50@gmail.com>
2022-04-10 19:51:27 +0200

    Initial Commit

---
+++ CTeam/CTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTeamApp.swift
+//  CTeam
+//
+//  Created by Andrea Autiero on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
